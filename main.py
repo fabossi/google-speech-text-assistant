73b8d9e6bef2246568f1c7cf8e3dd6c28db93fda
Felipe Fabossi <felipe.augusto.fabossi@ibm.com>
2020-06-25 09:29:41 -0300

    [FABOSSI] Google Assistant with search voice engine

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 import speech_recognition as sr
+from time import ctime
+import webbrowser
 import pyaudio
 
 p = pyaudio.PyAudio()
 r = sr.Recognizer()
 
 
-def record_audio():
-    
+def record_audio(ask=False):
+
     # This is to see wich microhone im using
 
     for i in range(p.get_device_count()):
         info = p.get_device_info_by_index(i)
-        print('Microphones: ', info['index'], info['name'])
+        # print('Microphones: ', info['index'], info['name'])
 
     with sr.Microphone() as source:
-        print('Say something => ')
+        if ask:
+            print(ask)
+
         audio = r.listen(source)
-        audio_data = r.recognize_google(audio)
+        voice_data = ''
 
         try:
             # for testing purposes, we're just using the default API key
@@ -25,9 +29,28 @@ def record_audio():
 
             # instead of `r.recognize_google(audio)`
 
-            print("Google Speech Recognition thinks you said => " + audio_data)
+            voice_data = r.recognize_google(audio)
+
         except sr.UnknownValueError:
             print("Google Speech Recognition could not understand audio")
         except sr.RequestError as e:
             print(
                 "Could not request results from Google Speech Recognition service; {0}".format(e))
+        return voice_data
+
+
+def respond(voice_data):
+    if 'what is your name' in voice_data:
+        print('My name is Alexis')
+    if 'What time is it' in voice_data:
+        print(ctime())
+    if 'search' in voice_data:
+        search = record_audio('what do you want to search for? ')
+        url = 'https://google.com/search?q=' + search
+        webbrowser.get().open(url)
+        print('Here is what I found for ' + search)
+
+
+print('How can I help you ? ')
+voice_data = record_audio()
+respond(voice_data)
